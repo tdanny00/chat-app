0391d39e26d04912c49b77b6741c443438fea40a
tdanny00 <tolgyesipontdani@gmail.com>
2024-05-03 21:19:10 +0200

    chat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDzyDZwPoYnghaDZ85TsFWrcrgV2J1HfQo',
+    authDomain: 'dannycord-48053.firebaseapp.com',
+    projectId: 'dannycord-48053',
+    storageBucket: 'dannycord-48053.appspot.com',
+    messagingSenderId: '358545549865',
+    appId: '1:358545549865:web:0910656871872e7116ca38',
+    measurementId: 'G-9DC1XEVCD8',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: 'AIzaSyDzyDZwPoYnghaDZ85TsFWrcrgV2J1HfQo',
     authDomain: 'dannycord-48053.firebaseapp.com',
@@ -12,7 +12,6 @@ export const environment = {
     messagingSenderId: '358545549865',
     appId: '1:358545549865:web:0910656871872e7116ca38',
     measurementId: 'G-9DC1XEVCD8',
-    production: true,
   },
 };
 
